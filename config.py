05ca9fb00f581da4b6c9210b6b10b5a90cd5a942
mdanishu <mdanishu92@gmail.com>
2023-04-02 03:20:25 -0400

    remove config

--- config.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-pwd1 = "donutking"
-alphavantage_key = "7E8P7TXUGYU3XNTT"
-quandl_key = "FSjK4XR_Ltmk5ia2QsGw"
-twelvedata_key = "1bb8f493a71f47fdbf53130f7a10f4a0"
-openai_key = "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"
